57301fc8fa64f6398a52c14ca2920657abb619f5
saaip7 <syaifullahhilmimaarij2003@mail.ugm.ac.id>
2024-05-18 17:43:50 +0700

    Add delayLED variable for controlling LED delay

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "sbm_athar_syaif.h"
 
 int potPin = 33;
+int delayLED = 25;
 
 void setup() {
   //inisialisasi pin yang akan digunakan
@@ -13,5 +14,5 @@ void setup() {
 
 // Fungsi untuk memutar loop
 void loop() {
-  sequence(27, 32, 21, 26, potensio(potPin, 300, 0));
+  sequence(27, 32, 21, 26, potensio(potPin, 300, 0)+delayLED);
 }
